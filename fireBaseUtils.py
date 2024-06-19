8098700ba91002f880901d9af27ae9c1b9ec3ef5
Liam <49714254+LiamHecht@users.noreply.github.com>
2024-06-20 01:35:01 +0300

    Update fireBaseUtils.py

--- fireBaseUtils.py
+++ fireBaseUtils.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 
 cred = credentials.Certificate("assets/serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://chess-6d839-default-rtdb.europe-west1.firebasedatabase.app/'
+    'databaseURL': ''
 })
 
 def check_username_in_documents(username):
